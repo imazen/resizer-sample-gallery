58e02a29c715cdeedb7fc84b8b66ca5f2c4fee1c
ecerta <eliz.certa@gmail.com>
2014-10-22 14:41:38 -0400

    view model and view page for the Purchase page

---
+++ src/ImageResizer.Samples.Gallery.Web/ViewModels/PurchasePageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ImageResizer.Samples.Gallery.Web.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace ImageResizer.Samples.Gallery.Web.ViewModels
+{
+    public class PurchasePageViewModel
+    {
+        public List<Image> Images { get; set; }
+
+        public PurchasePageViewModel()
+        {
+            string databasePath = HttpContext.Current.Server.MapPath("~/App_Data/Database.json");
+            using (StreamReader r = new StreamReader(databasePath))
+            {
+                string json = r.ReadToEnd();
+                Images = JsonConvert.DeserializeObject<List<Image>>(json);
+            }
+            Images = Images ?? new List<Image>();
+
+        }
+    }
+
+}
