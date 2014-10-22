aaab2c7900c4757993e21b05419a120276d79acc
ecerta <eliz.certa@gmail.com>
2014-10-22 14:39:38 -0400

    Added controller, viewmodel and view for Buy Stuff page

---
+++ src/ImageResizer.Samples.Gallery.Web/Controllers/BuyStuffController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageResizer.Samples.Gallery.Web.Controllers
+{
+    public class BuyStuffController : Controller
+    {
+        // GET: BuyStuff
+        public ActionResult Index()
+        {
+            return View(new ImageResizer.Samples.Gallery.Web.ViewModels.BuyStuffViewModel());
+        }
+    }
+}
