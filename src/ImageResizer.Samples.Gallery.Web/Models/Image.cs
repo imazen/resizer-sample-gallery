e065f998fa0a8b6f5d6a759f34001e2dd6b6cd2d
ecerta <eliz.certa@gmail.com>
2014-10-22 14:10:48 -0400

    Added Description get and set, as well as cropping capabilities

--- src/ImageResizer.Samples.Gallery.Web/Models/Image.cs
+++ src/ImageResizer.Samples.Gallery.Web/Models/Image.cs
@@ -5,5 +5,20 @@ namespace ImageResizer.Samples.Gallery.Web.Models {
         public Guid Id { get; set; }
         public string FileName { get; set; }
         public string Author { get; set; }
+        public int StoredWidth { get; set; }
+        public int StoredHeight { get; set; }
+        public string EditQuery { get; set; }
+        public string MimeType { get; set; }
+        public string Description { get; set; }
+
+        public string GetQueryForCropping(int? defaultWidth)
+        {
+            var i = new Instructions(EditQuery ?? "");
+            i.CropXUnits = null;
+            i.CropYUnits = null;
+            i.CropRectangle = null;
+            i.Width = defaultWidth;
+            return i.ToQueryString();
+        }
     }
 }
