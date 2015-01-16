e27f45f392d7fca46d90222cd6ea094c012c44b1
ecerta <eliz.certa@gmail.com>
2015-01-16 17:37:43 -0500

    Starting work on second upload link

--- src/ImageResizer.Samples.Gallery.Web/Controllers/HomeController.cs
+++ src/ImageResizer.Samples.Gallery.Web/Controllers/HomeController.cs
@@ -75,5 +75,61 @@ namespace ImageResizer.Samples.Gallery.Web.Controllers
             //TODO: they didn't attach a file. Complain, or something.
             return RedirectToAction("index", "home");
         }
+        /*public ActionResult Upload2()
+        {
+            Image image = null;
+
+            if (Request.Files.Keys.Count == 1)
+            {
+                string name = Request.Files.Keys[0];
+                var httpPostedFile = Request.Files[name];
+
+                //Get image dimensions
+                httpPostedFile.InputStream.Seek(0, SeekOrigin.Begin);
+                var originalInfo = ImageResizer.ImageBuilder.Current.LoadImageInfo(httpPostedFile, null);
+
+
+                // Save the original file
+                var imageUploader = new ImageUploader();
+                var fileName = imageUploader.SaveUploadedFileSafely(
+                    baseDir: "~/Content/Images/Uploads/",
+                    uploadFile: httpPostedFile,
+                    whitelistedFormats: new string[] {
+                        "gif", "jpg", "png", "tif"
+                    }
+                );
+
+
+
+                image = new Image
+                {
+                    Id = new Guid(Path.GetFileNameWithoutExtension(fileName)),
+                    FileName = fileName,
+                    Author = Request["author"],
+                    Description = Request["description"],
+                    StoredWidth = (int)originalInfo["source.width"],
+                    StoredHeight = (int)originalInfo["source.height"],
+                };
+
+                // Limit size, convert to jpg, and autorotate
+                httpPostedFile.InputStream.Seek(0, SeekOrigin.Begin);
+                var imageJob = new ImageJob(
+                    httpPostedFile,
+                    "~/Content/Images/Uploads/" + image.Id.ToString("N", NumberFormatInfo.InvariantInfo) + "_500x500.<ext>",
+                    new Instructions("width=500;height=500;format=jpg;mode=max;autorotate=true;"));
+                imageJob.CreateParentDirectory = true;
+                imageJob.Build();
+
+                //Save image record to database
+                var saveImageQuery = new SaveImageQuery();
+                saveImageQuery.Execute(image);
+
+                return RedirectToAction("crop", "images", new RouteValueDictionary {
+                    { "id", image.Id }
+                });
+            }
+            //TODO: they didn't attach a file. Complain, or something.
+            return RedirectToAction("index", "home");
+        }*/
     }
 }
